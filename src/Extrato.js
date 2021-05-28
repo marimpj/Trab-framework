c37f22785d0157c68073503a0b45be141bde0e34
lucasfacci <50666911+lucasfacci@users.noreply.github.com>
2021-05-27 22:18:12 -0300

    Update extrato e lancamento

--- src/Extrato.js
+++ src/Extrato.js
@@ -4,133 +4,147 @@ import { Card, Table, Button, Progress } from 'antd'
 import Head from './Header'
 import { Link } from 'react-router-dom'
 
-const dataSource0 = [
-    {
-      key: '1' ,
-      data: '12/12/2021',
-      categoria: 'Categoria: Alimentação',
-      valor: 'R$ 35,00'
-    },
-    {
-      key: '2',
-      data: '12/12/2021',
-      categoria: 'Categoria: Lazer',
-      valor: 'R$ 35,00'
-    },
-];
-  
-const columns0 = [
-    {
-        title: 'Data',
-        dataIndex: 'data',
-        key: 'data',
-    },
-    {
-        title: 'Categoria',
-        dataIndex: 'categoria',
-        key: 'categoria',
-    },
-    {
-        title: 'Valor',
-        dataIndex: 'valor',
-        key: 'valor',
-    },
-    {
-        title: '',
-        dataIndex: 'excluir',
-        key: 'excluir',
-        render: () => (
-            <button onClick={()=> console.log("Funcionando")}>
-              Excluir
-            </button>
-        ),
-    },
-];
+export default function Gastos(props) {
 
-const dataSource1 = [
-    {
-      key: '1',
-      gasto: 'Total gasto em transporte:',
-      valor: 'R$ 35,00'
-    },
-    {
-      key: '2',
-      gasto: 'Total gasto em lazer:',
-      valor: 'R$ 70,00'
-    },
-    {
-      key: '3',
-      gasto: 'Total gasto em alimentação:',
-      valor: 'R$ 175,00'
-    },
-    {
-      key: '4',
-      gasto: 'Total geral:',
-      valor: 'R$ 280,00'
-    },
-];
-  
-const columns1 = [
-    {
-        title: '',
-        dataIndex: 'gasto',
-        key: 'gasto',
-    },
-    {
-        title: '',
-        dataIndex: 'valor',
-        key: 'valor',
-    },
-];
+    let url_novo = `https://7c2bad50.us-south.apigw.appdomain.cloud/api/gasto?username=${localStorage.getItem("username")}`
 
-export default class Gastos extends React.Component {
+    const itemsList = []
 
-    // const listarGastos = () => {
+    const listarGastos = () => {
+        axios.get(url_novo).then((resp) => {
 
-    //     let URL = "https://7c2bad50.us-south.apigw.appdomain.cloud/api/gasto?username=edson"
-    //     axios.get(URL).then((resp) => {
-            
-    //         console.log(resp.data)
-    //         resp.data.gastos.forEach((item, idx) => console.log(item.categoria, Date(item.data)))
+            resp.data.gastos.forEach((item, idx) => {
+                let single = {
+                    key: `${idx}`,
+                    data: `${Date(item.data)}`,
+                    categoria: `${item.categoria}`,
+                    valor: `R$ ${item.valor}`
+                }
+                itemsList.push(single)
+            })
 
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     })
+        })
+        .then(() => {
+            console.log(itemsList)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
 
-    // }
+    const apagar = () => {
+        axios.delete(url_novo + "?id=b3dc39952eb0ef0386763344c5c4bdc7&rev=1-0612ec1b12e30121942cd6f4587ac4ed").then((resp) => {
+            console.log(resp.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+    
+    listarGastos()
 
-    // const apagar = () => {
+    const dataSource0 = [
+        {
+          key: '1' ,
+          data: '12/12/2021',
+          categoria: 'Categoria: Alimentação',
+          valor: 'R$ 35,00'
+        },
+        {
+          key: '2',
+          data: '12/12/2021',
+          categoria: 'Categoria: Lazer',
+          valor: 'R$ 35,00'
+        },
+    ];
+      
+    const columns0 = [
+        {
+            title: 'Data',
+            dataIndex: 'data',
+            key: 'data',
+        },
+        {
+            title: 'Categoria',
+            dataIndex: 'categoria',
+            key: 'categoria',
+        },
+        {
+            title: 'Valor',
+            dataIndex: 'valor',
+            key: 'valor',
+        },
+        {
+            title: '',
+            dataIndex: 'excluir',
+            key: 'excluir',
+            render: () => (
+                <button onClick={()=> console.log("Funcionando")}>
+                  Excluir
+                </button>
+            ),
+        },
+    ];
+    
+    const dataSource1 = [
+        {
+          key: '1',
+          gasto: 'Total gasto em transporte:',
+          valor: 'R$ 35,00'
+        },
+        {
+          key: '2',
+          gasto: 'Total gasto em lazer:',
+          valor: 'R$ 70,00'
+        },
+        {
+          key: '3',
+          gasto: 'Total gasto em alimentação:',
+          valor: 'R$ 175,00'
+        },
+        {
+          key: '4',
+          gasto: 'Total geral:',
+          valor: 'R$ 280,00'
+        },
+    ];
+      
+    const columns1 = [
+        {
+            title: '',
+            dataIndex: 'gasto',
+            key: 'gasto',
+        },
+        {
+            title: '',
+            dataIndex: 'valor',
+            key: 'valor',
+        },
+    ];
 
-    //     axios.delete(url_novo + "?id=b3dc39952eb0ef0386763344c5c4bdc7&rev=1-0612ec1b12e30121942cd6f4587ac4ed").then((resp) => {
-    //         console.log(resp.data)
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     })
-    // }
+    console.log(dataSource0)
+    console.log(itemsList)
 
-    render() {
-        return (
-            <div>
-                <Head/>
-                <div style={{textAlign:"center"}}>
-                    <Link to="/lancamentos">
-                        <Button style={{marginTop: "10px", color: "white", border: "2px solid #FAFAFA", width:"auto", height:"auto", fontSize: "110%"}} type="primary" ghost>Fazer um lançamento</Button>
-                    </Link>
-                </div>
-                <div style={{display:"flex", justifyContent:'center', marginTop: "30px"}}>
-                    <Card>
-                    <h1 style={{color: "black", background: "white", fontSize:"20px", textAlign:"center", marginTop:"-15px", marginBottom:"25px"}} >Extrato</h1>
-                        <Table dataSource={dataSource0} columns={columns0} pagination={false} />
-                        <Table dataSource={dataSource1} columns={columns1} pagination={false} />
-                    </Card>
-                </div>
-                <div style={{display:"flex", justifyContent:'center', color: "white", marginTop: "20px", fontWeight:"bold", fontSize:"115%"}}>Valor alcançado da meta:</div>
-                <div style={{display:"flex", justifyContent:'center', color: "white"}}>
-                    <div style={{padding: "30px" }}>
-                        <Progress type="circle" percent={30} />
-                    </div>
+    return (
+        <div>
+            <Head/>
+            <div style={{textAlign:"center"}}>
+                <Link to="/lancamentos">
+                    <Button style={{marginTop: "10px", color: "white", border: "2px solid #FAFAFA", width:"auto", height:"auto", fontSize: "110%"}} type="primary" ghost>Fazer um lançamento</Button>
+                </Link>
+            </div>
+            <div style={{display:"flex", justifyContent:'center', marginTop: "30px"}}>
+                <Card>
+                <h1 style={{color: "black", background: "white", fontSize:"20px", textAlign:"center", marginTop:"-15px", marginBottom:"25px"}} >Extrato</h1>
+                    <Table dataSource={itemsList} columns={columns0} pagination={false} />
+                    <Table dataSource={dataSource1} columns={columns1} pagination={false} />
+                </Card>
+            </div>
+            <div style={{display:"flex", justifyContent:'center', color: "white", marginTop: "20px", fontWeight:"bold", fontSize:"115%"}}>Valor alcançado da meta:</div>
+            <div style={{display:"flex", justifyContent:'center', color: "white"}}>
+                <div style={{padding: "30px" }}>
+                    <Progress type="circle" percent={30} />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
--- src/Lancamentos.js
+++ src/Lancamentos.js
@@ -1,33 +1,41 @@
 import React from 'react'
 import axios from 'axios'
 import { Button, Form, Input, Card } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import{ Layout, Select, Option } from'antd'
 import Head from './Header'
 
-const{ Header} = Layout;
+const{ Header } = Layout;
 
 export default function Lancamentos(props) {
 
     const url_novo = "https://7c2bad50.us-south.apigw.appdomain.cloud/api/gasto"
 
+    const historyLancamento = useHistory()
+
     const { Option } = Select;
         function handleChange(value) {
-        console.log(`selected ${value}`);
     }
 
     const enviar = (dados) => {
-        let gasto = new Object();
-        gasto.username = localStorage.getItem("username")
-        gasto.timestamp = new Date().getTime()
-        gasto.categoria = dados.categoria
-        gasto.valor = dados.valor
-        let jsonString = JSON.stringify(gasto);
-        axios.post(url_novo, jsonString).then((resp) => {
-            console.log(resp.data)
-        }).catch((err) => {
-            console.log(err)
-        })
+        if (isNaN(dados.valor)) {
+            alert("O valor deve ser um número.");
+        } else {
+            let gasto = new Object();
+            gasto.username = localStorage.getItem("username")
+            gasto.categoria = dados.categoria
+            gasto.valor = parseInt(dados.valor)
+            gasto.data = new Date().getTime()
+            axios.post(url_novo, gasto).then((resp) => {
+                if (resp.data.ok == true) {
+                    historyLancamento.push('/extrato')
+                } else {
+                    alert('Erro: Alguma informação inserida encontra-se incorreta!')
+                }
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
     }
 
     const erro = (err) => {
@@ -43,28 +51,28 @@ export default function Lancamentos(props) {
                 </Link>
             </div>
             <div style={{display:"flex", justifyContent:'center', marginTop: "10px"}}>
-            <Card>
-            <Header style={{color: "black", background: "white", fontSize:"20px", textAlign:"center", marginTop:"-15px", marginBottom:"25px"}} >Faça o seu Lançamento</Header>
-                <Form name='lancamentos' onFinish={enviar} onFinishFailed={erro}>
-                <Form.Item style={{color: "white"}} label="Valor" name="valor"
-                rules={[ { required: true, message: 'Informe o cadastro' } ]}>
-                <Input />
-                </Form.Item>
-                <Form.Item label="Categoria" name="categoria"
-                rules={[ { required: true, message: 'Informe a categoria' } ]}>
-                <Select defaultValue="alimentacao" style={{ width: 120 }} onChange={handleChange}>
-                <Option value="alimentacao">Alimentação</Option>
-                <Option value="lazer">Lazer</Option>
-                <Option value="transporte">Transporte</Option>
-                </Select>
-                </Form.Item>
-                <Form.Item>
-                <div style={{display:"flex", justifyContent:'center'}}>
-                    <Button  type="primary" ghost htmlType="submit">Enviar</Button>
-                </div>
-                </Form.Item>
-                </Form>
-            </Card>
+                <Card>
+                    <Header style={{color: "black", background: "white", fontSize:"20px", textAlign:"center", marginTop:"-15px", marginBottom:"25px"}} >Faça o seu Lançamento</Header>
+                    <Form name='lancamentos' onFinish={enviar} onFinishFailed={erro}>
+                        <Form.Item style={{color: "white"}} label="Valor" name="valor"
+                        rules={[ { required: true, message: 'Informe o cadastro' } ]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item label="Categoria" name="categoria"
+                        rules={[ { required: true, message: 'Informe a categoria' } ]}>
+                            <Select style={{ width: 120 }} onChange={handleChange}>
+                                <Option value="alimentacao">Alimentação</Option>
+                                <Option value="lazer">Lazer</Option>
+                                <Option value="transporte">Transporte</Option>
+                            </Select>
+                        </Form.Item>
+                        <Form.Item>
+                            <div style={{display:"flex", justifyContent:'center'}}>
+                                <Button  type="primary" ghost htmlType="submit">Enviar</Button>
+                            </div>
+                        </Form.Item>
+                    </Form>
+                </Card>
             </div>
         </div>
     )
